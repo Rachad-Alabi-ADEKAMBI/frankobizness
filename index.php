ea12c55b4f768ce4af38a3b8d929076b5ad82cac
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2022-11-10 20:13:12 +0100

    update files

---
+++ index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frankobizness - Accueil</title>
+    <link rel="stylesheet" href="public/scss/main.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+</head>
+<body>
+    <div class="main">
+        <header class='header'>
+
+        </header>
+
+        <div class="search">
+            <div class="search__item">
+            <p>
+                <span>
+                Chercher un vehicule
+                </span> <br>
+                Faites une recherche
+            </p>
+            </div>
+
+            <div class="search__item">
+                <label for="">
+                    Marque <br>
+                    <input type="text">
+                </label>
+
+                <label for="">
+                    Prix min: <br>
+                    <input type="text">
+                </label>
+
+                <label for="">Prix max
+                <input type="text">
+                </label>
+            </div>
+        </div>
+
+        <section>
+            <h2>
+                Derniers ajouts
+            </h2>
+
+            <div class="items">
+                <div class="item">
+                    <div class="item__top">
+                        <img src="" alt="">
+                        <div class="info">A vendre</div>
+                        <div class="bar">
+                            <p>A vendre</p>
+                        </div>
+                    </div>
+
+                    <div class="item__bottom">
+                        <h3>
+                            Mercedes Benz
+                        </h3>
+
+                        <p class="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nemo ab enim consequatur? Totam provident recusandae reiciendis autem, minima id
+                            consequuntur excepturi doloremque eveniet ab esse voluptates ex, placeat est quo?
+                        </p>
+
+                        <ul>
+                            <li>
+                                Annee: <span>2010</span>
+                            </li>
+
+                            <li>
+                                Etat: <span>9/10</span>
+                            </li>
+
+                            <li>
+                                Couleur: <span></span>
+                            </li>
+                        </ul>
+
+                        <p class="price">
+                            2 000 000F CFA
+                        </p>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+        </section>
+
+        <section>
+            <h2>
+                Les plus consultees
+            </h2>
+
+            <div class="slider">
+
+            </div>
+        </section>
+
+        <section>
+            <h2>
+                Derniers ajouts
+            </h2>
+
+            <br>
+            <div class="items">
+                <div class="item">
+                    <div class="item__top">
+                        <img src="" alt="">
+                        <div class="info"></div>
+                        <div class="bar">
+                            <p>A louer</p>
+                        </div>
+                    </div>
+
+                    <div class="item__bottom">
+                        <h3>
+                            Toyota RR
+                        </h3>
+
+                        <p class="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nemo ab enim consequatur? Totam provident recusandae reiciendis autem, minima id
+                            consequuntur excepturi doloremque eveniet ab esse voluptates ex, placeat est quo?
+                        </p>
+
+                        <ul>
+                            <li>
+                                Annee: <span>2010</span>
+                            </li>
+
+                            <li>
+                                Etat: <span>9/10</span>
+                            </li>
+
+                            <li>
+                                Couleur: <span>Verte</span>
+                            </li>
+                        </ul>
+
+                        <p class="price">
+                            50 000 FCFA /JOUR
+                        </p>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+        </section>
+
+        <section>
+            <h2>
+                Frankobizness <br>
+                <span>Votre satisfaction importe</span>
+            </h2>
+
+           <div class="cards">
+                    <div class="card">
+                        <p>icon</p>
+
+                        <p>
+                            Prix accessibles
+                        </p>
+
+                        <p>
+                            Nos prix sont tres etudies
+                        </p>
+                    </div>
+
+                    <div class="card">
+                        <p>icon</p>
+
+                        <p>
+                            Prix accessibles
+                        </p>
+
+                        <p>
+                            Nos prix sont tres etudies
+                        </p>
+                    </div>
+
+                    <div class="card">
+                        <p>icon</p>
+
+                        <p>
+                            Prix accessibles
+                        </p>
+
+                        <p>
+                            Nos prix sont tres etudies
+                        </p>
+                    </div>
+            </div>
+        </section>
+
+        <div class="black">
+            <div class="black__content">
+                <p>Trouvez votre vehicule aujourdh'hui </p>
+                <button>
+                    Chercher
+                </button>
+            </div>
+        </div>
+
+        <section>
+            <div class="testimonies">
+                <h2>
+                    Temoignages
+                </h2>
+
+                <div class="testimonies__content">
+                    <div class="testimony">
+                        <img src="" alt="">
+                        <p class="author">
+                            Marius A.
+                        </p>
+
+                        <p class="title">
+                            client satisfait
+                        </p>
+
+                        <p>
+                            Je suis tres satisfait de mon achat, je recommande vivement
+                            Frankobizness
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <h2>
+                Dernieres ventes
+            </h2>
+
+            <br>
+            <div class="items">
+                <div class="item">
+                    <div class="item__top">
+                        <img src="" alt="">
+                        <div class="info"></div>
+                        <div class="bar">
+                            <p>Vendu</p>
+                        </div>
+                    </div>
+
+                    <div class="item__bottom">
+                        <h3>
+                            Volswagen
+                        </h3>
+
+                        <p class="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nemo ab enim consequatur? Totam provident recusandae reiciendis autem, minima id
+                            consequuntur excepturi doloremque eveniet ab esse voluptates ex, placeat est quo?
+                        </p>
+
+                        <ul>
+                            <li>
+                                Annee: <span>2010</span>
+                            </li>
+
+                            <li>
+                                Etat: <span>9/10</span>
+                            </li>
+
+                            <li>
+                                Couleur: <span></span>
+                            </li>
+                        </ul>
+
+                        <p class="price">
+                            3 500 000F CFA
+                        </p>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+        </section>
+
+        <footer class="footer">
+            <div class="footer__top">
+                <div class="box">
+                    <h4>
+                        Frankobizness
+                    </h4>
+
+                    <p>
+                        Vente et locations de vehicules
+                    </p>
+
+                    <p>9722412</p>
+                    <p>Facebook</p>
+                    <p>Instagram</p>
+                    <p>contact@frankobizness.com</p>
+                </div>
+
+                <div class="box">
+                    <h4>
+                        Les plus consultees
+                    </h4>
+
+                    <div class="cars">
+                        <div class="car">
+                            <img src="" alt="">
+                            <h5>Voiture 1 <br>
+                        <span>4 000 000 FCFA</span></h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <h4>
+                        Liens
+                    </h4>
+
+                    <ul>
+                        <li>
+                            A louer
+                        </li>
+
+                        <li>
+                            A vendre
+                        </li>
+
+                        <li>
+                            Derniers ajouts
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="box">
+                    <h4>
+                        Newsletters
+                    </h4> <br>
+                    <p>
+                        Abonnez vous pour recevoir chaque nouvel ajout
+                    </p>
+                </div>
+            </div>
+
+            <hr>
+            <div class="footer__bottom">
+                <p>Frankobizness2022</p>
+                <p>
+                    Codeurcreatif
+                </p>
+
+            </div>
+        </footer>
+
+    </div>
+</body>
+</html>
